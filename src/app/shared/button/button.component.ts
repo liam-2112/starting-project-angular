79d9b42e990e3c04ed77a74acf5b3f67440b5b1c
liam-chadwick <liam_2112@hotmail.co.uk>
2025-06-23 14:01:16 +0100

    content projection with multiple slots

--- src/app/shared/button/button.component.ts
+++ src/app/shared/button/button.component.ts
@@ -7,5 +7,4 @@ import { Component } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrl: './button.component.css'
 })
-export class ButtonComponent {
-}
+export class ButtonComponent {}
